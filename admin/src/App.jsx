74a3b6b18428a8b236e703f89def11ec017fd02c
ghaithkhl27 <ghaithkhalfallah27@hotmail.com>
2024-11-04 18:19:54 +0100

    test

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import './App.css';
+import Login from './auth/login'; 
+import Signup from './auth/signup'; 
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-        </h1>
-    </>
-  )
+  const login =window.localStorage.getItem("islogedIn")
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ login ? <Dashboard /> :<Login />} /> 
+                    <Route path="/signup" element={<Signup />} /> 
+                    
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App; 
