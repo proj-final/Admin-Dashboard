e88a9cb1e4686bd78936d08ae4af015954b5fcfc
firas2200 <firaskhalifa4@gmail.com>
2024-11-05 12:06:51 +0000

    side bar

--- admin/src/Dashboard/Sidebar.jsx
+++ admin/src/Dashboard/Sidebar.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaUser, FaUtensils, FaMotorcycle, FaClipboardList } from 'react-icons/fa'; 
+import { FaUser, FaUtensils, FaMotorcycle, FaClipboardList } from 'react-icons/fa';
 
-const Sidebar = () => {
-    return (
-        <aside className="bg-orange-500 text-white w-64 h-full fixed top-0 left-0 pt-16 flex flex-col">
-            <div className="p-4">
-            </div>
-            <nav className="flex flex-col mt-8 space-y-4 p-4">
-                <Link to="/client" className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg">
-                    <FaUser className="text-lg" /> 
-                    <span>Client</span>
-                </Link>
-                <Link to="/chefs" className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg">
-                    <FaUtensils className="text-lg" /> 
-                    <span>Chefs</span>
-                </Link>
-                <Link to="/delivery-boy" className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg">
-                    <FaMotorcycle className="text-lg" />
-                    <span>Delivery Boy</span>
-                </Link>
-                <Link to="/order" className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg">
-                    <FaClipboardList className="text-lg" /> 
-                    <span>Order</span>
-                </Link>
-            </nav>
-        </aside>
-    );
+const Sidebar = ({ onMenuSelect }) => {
+  return (
+    <aside className="bg-orange-500 text-white w-64 h-full fixed top-0 left-0 pt-16 flex flex-col">
+      <div className="p-4">
+        <h1 className="text-3xl font-bold">Admin</h1>
+      </div>
+      <nav className="flex flex-col mt-8 space-y-4 p-4">
+        <div onClick={() => onMenuSelect('Client')} className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg cursor-pointer">
+          <FaUser className="text-lg" />
+          <span>Client</span>
+        </div>
+        <div onClick={() => onMenuSelect('Chief')} className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg cursor-pointer">
+          <FaUtensils className="text-lg" />
+          <span>Chefs</span>
+        </div>
+        <div onClick={() => onMenuSelect('Delivery Boy')} className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg cursor-pointer">
+          <FaMotorcycle className="text-lg" />
+          <span>Delivery Boy</span>
+        </div>
+        <div onClick={() => onMenuSelect('Order')} className="flex items-center space-x-2 hover:bg-orange-600 p-2 rounded-lg cursor-pointer">
+          <FaClipboardList className="text-lg" />
+          <span>Order</span>
+        </div>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
